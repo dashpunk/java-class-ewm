8fc4255aa49cda9e719c8da85e145fa562e43e14
willians.lemos87@gmail.com <willians.lemos87@gmail.com@80753d7a-18a9-1890-8868-072d0b567191>
2013-12-18 11:42:40 +0000

    Classe não utilizada no maximo.

--- MAXIMOID2/src/br/inf/ctis/ms/bean/MsDemJud.java
+++ MAXIMOID2/src/br/inf/ctis/ms/bean/MsDemJud.java
@@ -1,12 +1,6 @@
-/**
- * 
- */
 package br.inf.ctis.ms.bean;
 
-import java.rmi.RemoteException;
-
-import psdi.mbo.MboRemote;
-import psdi.util.MXException;
+import psdi.webclient.system.beans.DataBean;
 
 /**
  * @author willians.andrade
@@ -18,23 +12,13 @@ public class MsDemJud extends
 	public MsDemJud() {
 		System.out.print("CTIS # --- Entrou na Classe AppBean MsDemJud");
 	}
-
-	public void save() throws MXException {
-
-		try {
-			System.out.print("CTIS # --- Entrou no Save()");
-			MboRemote mboMedicamentos = null;
-			
-			for (int i = 0; ((mboMedicamentos = getMbo().getMboSet("MSTBMEDICAMENTO").getMbo(i)) != null); i++) {
-				mboMedicamentos.setValue("SELECIONADO", false);
-			}
-			super.save();
-			
-		} catch (RemoteException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
+	
+	
+	@Override
+	public synchronized void dataChangedEvent(DataBean speaker) {
+		reloadTable();
+		refreshTable();
+		
+		super.dataChangedEvent(speaker);
 	}
-
 }
