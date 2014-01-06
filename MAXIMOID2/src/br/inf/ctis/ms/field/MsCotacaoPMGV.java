75319f5a48aeaef422079c373f1f658ab35e553b
willians.lemos87@gmail.com <willians.lemos87@gmail.com@80753d7a-18a9-1890-8868-072d0b567191>
2014-01-06 10:41:19 +0000

    Calcular o valor do PGMV Unitário

--- MAXIMOID2/src/br/inf/ctis/ms/field/MsCotacaoPMGV.java
+++ MAXIMOID2/src/br/inf/ctis/ms/field/MsCotacaoPMGV.java
@@ -6,9 +6,9 @@ package br.inf.ctis.ms.field;
 
 import java.rmi.RemoteException;
 
+import psdi.id2.Uteis;
 import psdi.mbo.MboConstants;
 import psdi.mbo.MboSet;
-import psdi.mbo.MboSetRemote;
 import psdi.mbo.MboValue;
 import psdi.mbo.MboValueAdapter;
 import psdi.util.MXApplicationException;
@@ -35,10 +35,7 @@ public class MsCotacaoPMGV extends MboValueAdapter {
 		Double Fator = getMboValue().getMbo().getDouble("MSNUMFATEMB");
 		Double Qntd = getMboValue().getMbo().getDouble("MSNUMQNT");
 		
-		MboSetRemote Medicamento;
-		Medicamento = getMboValue().getMbo().getMboSet("MSTBCOTCDJU");
-		
-		Double MedicamentoQntd = Medicamento.getDouble("MSQTD");
+		Double MedicamentoQntd = getMboValue().getMbo().getMboSet("MSTBCOTCDJU").getMbo(0).getDouble("MSQTD");
 		
 		if(!getMboValue().getMbo().isNull("MSNUMFATEMB") && !getMboValue().getMbo().isNull("MSNUMQNT")){
 			Double Total = Qntd / Fator;
@@ -56,46 +53,45 @@ public class MsCotacaoPMGV extends MboValueAdapter {
 		 */
 		MboSet mboSetPMVG;
 		mboSetPMVG = (MboSet) psdi.server.MXServer.getMXServer().getMboSet("MSTBGGREM", getMboValue().getMbo().getUserInfo());
-		mboSetPMVG.setWhere("MSTBGGREMID = '" + getMboValue().getMbo().getString("MSTBGGREMID"));
+		System.out.println("##### CTIS MsCotacaoPMGV -  MSTBGGREMID: " + Uteis.getApenasNumeros(getMboValue().getMbo().getString("MSTBGGREMID")));
+		mboSetPMVG.setWhere("MSTBGGREMID = '" + Uteis.getApenasNumeros(getMboValue().getMbo().getString("MSTBGGREMID")) + "'");
 		mboSetPMVG.reset();
 		
-		Double PMGV_0 = mboSetPMVG.getMbo().getDouble("PMVG_0");
-		Double PMGV_12 = mboSetPMVG.getMbo().getDouble("PMVG_12");
-		Double PMGV_17 = mboSetPMVG.getMbo().getDouble("PMVG_17");
-		Double PMGV_18 = mboSetPMVG.getMbo().getDouble("PMVG_18");
-		Double PMGV_19 = mboSetPMVG.getMbo().getDouble("PMVG_19");		
+		Double PMGV_0 = mboSetPMVG.getMbo(0).getDouble("PMVG_0");
+		Double PMGV_12 = mboSetPMVG.getMbo(0).getDouble("PMVG_12");
+		Double PMGV_17 = mboSetPMVG.getMbo(0).getDouble("PMVG_17");
+		Double PMGV_18 = mboSetPMVG.getMbo(0).getDouble("PMVG_18");
+		Double PMGV_19 = mboSetPMVG.getMbo(0).getDouble("PMVG_19");	
+		
 		/*
 		 * Pessoa
 		 */
-		MboSetRemote Person;
-		Person = getMboValue().getMbo().getMboSet("PERSON");
+
+		String StateProvince = getMboValue().getMbo().getMboSet("PERSON").getMbo(0).getString("STATEPROVINCE");
 		
-		String StateProvince = Person.getString("STATEPROVINCE");
-		
-		if (getMboValue().getMbo().getString("MSALNCMED").equals("SIM")){
+		if (getMboValue().getMbo().getString("MSALNCMED").equals("SIM") && !getMboValue().getMbo().isNull("MSNUMFATEMB")){
 			/*
 			 * Se Campo Convênio <NAO> preencher PMGV Unitário igual ol PMVG_X da MSTBGGREM.
 			 */
-			if (getMboValue().getMbo().getString("MSALNCONV").equals("NÃO")){
+			if (getMboValue().getMbo().getString("MSALNCONV").equals("SIM")){
 				
 				System.out.println("#### CTIS - PMGV: " + PMGV_0);
-				getMboValue().getMbo().setValue("MSNUMPMVG",  PMGV_0, MboConstants.NOACCESSCHECK);
+				getMboValue().getMbo().setValue("MSNUMPMVG",  PMGV_0 / Fator, MboConstants.NOACCESSCHECK);
 			} else if (getMboValue().getMbo().getString("MSALNGENERICO").equals("SIM") && (StateProvince.equals("MG") || StateProvince.equals("PR"))){
 				System.out.println("#### CTIS - PMGV: " + PMGV_12);
-				getMboValue().getMbo().setValue("MSNUMPMVG",  PMGV_12, MboConstants.NOACCESSCHECK);
+				getMboValue().getMbo().setValue("MSNUMPMVG",  PMGV_12 / Fator, MboConstants.NOACCESSCHECK);
 			} else {
 				if(StateProvince.equals("RJ")){
 					System.out.println("#### CTIS - PMGV: " + PMGV_19);
-					getMboValue().getMbo().setValue("MSNUMPMVG",  PMGV_19, MboConstants.NOACCESSCHECK);
+					getMboValue().getMbo().setValue("MSNUMPMVG",  PMGV_19 / Fator, MboConstants.NOACCESSCHECK);
 				} else if (StateProvince.equals("SP")){
 					System.out.println("#### CTIS - PMGV: " + PMGV_18);
-					getMboValue().getMbo().setValue("MSNUMPMVG",  PMGV_18, MboConstants.NOACCESSCHECK);
+					getMboValue().getMbo().setValue("MSNUMPMVG",  PMGV_18 / Fator, MboConstants.NOACCESSCHECK);
 				} else {
 					System.out.println("#### CTIS - PMGV: " + PMGV_17);
-					getMboValue().getMbo().setValue("MSNUMPMVG",  PMGV_17, MboConstants.NOACCESSCHECK);
+					getMboValue().getMbo().setValue("MSNUMPMVG",  PMGV_17 / Fator, MboConstants.NOACCESSCHECK);
 				}
 			}
-		}
-		
+		}		
 	}
 }
