8bc667b84456136736795b6852dfd8249bdf2294
willians.lemos87 <willians.lemos87@gmail.com>
2014-03-19 13:49:32 +0000

    Classe Atualizada do PEC Versao 2

--- MAXIMOID2/src/br/inf/ctis/ms/bean/MsDistPEC.java
+++ MAXIMOID2/src/br/inf/ctis/ms/bean/MsDistPEC.java
@@ -5,9 +5,11 @@ package br.inf.ctis.ms.bean;
 
 import java.rmi.RemoteException;
 
-import com.sun.enterprise.util.Utility;
-
+import psdi.mbo.MboConstants;
 import psdi.mbo.MboRemote;
+import psdi.mbo.MboSet;
+import psdi.mbo.MboSetRemote;
+import psdi.util.MXApplicationException;
 import psdi.util.MXException;
 import psdi.webclient.system.beans.*;
 import psdi.webclient.system.controller.WebClientEvent;
@@ -21,67 +23,162 @@ import psdi.workflow.WorkflowDirector;
  */
 
 public class MsDistPEC extends DataBean {
-	public MsDistPEC() {
-		System.out.println("-------------------- PEC | Entrou na Classe MsDistPEC.");
+
+	String camposObrigatorios = "";
+	
+	int PoNum = 0; 
+	int MsFluxo = 0;
+	int MsAcao = 0; 
+	String MsGrupo = ""; 
+	String MSFLAGMSG = "0";
+
+	public MsDistPEC() {	
 	}
 
 	public int selectrecord() throws MXException, RemoteException {
 
-		WebClientEvent event = this.app.getWebClientSession().getCurrentEvent();
-		int row = getRowIndexFromEvent(event);
-
-		System.out.println("-------------------- PEC | Linha Selecionada DESCRICAO: " + getMbo(row).getString("DESCRIPTION"));
-		System.out.println("-------------------- PEC | Linha Selecionada ID FILHO: " + getMbo(row).getInt("MSTBPEC_OPCOESID"));
-		System.out.println("-------------------- PEC | Linha Selecionada PAI: " + getMbo(row).getInt("MSTBPEC_ACOESID"));
-		System.out.println("-------------------- PEC | Linha Selecionada DESTINO: " + getMbo(row).getInt("MSACAO"));
-
-		// Setar Valores na PO
+		super.selectrecord();
 		
+		
+		app.getDataBean("MAINRECORD").getMbo().setValue("MSFLAGMSG", "0", MboConstants.NOACCESSCHECK);
+		save();
+		
+		
+		linhaSelecionada();
+		validarCampos();
+		setarValores();
+		fecharERotear();
+
+		return 1;
+	}
+
+	private void linhaSelecionada() throws RemoteException, MXException  {
+				WebClientEvent event = this.app.getWebClientSession().getCurrentEvent();
+				int row = getRowIndexFromEvent(event);
+
 				MboRemote mboAcao;
 				mboAcao = getMbo().getMboSet("MSTBPEC_ACOES").getMbo(0);
-				
+
 				MboRemote mboGrupo;
 				mboGrupo = getMbo().getMboSet("MSTBPEC_GRUPO").getMbo(0);
-				
-				int PoNum = app.getDataBean("MAINRECORD").getMbo().getInt("PONUM"); // Número da PO
-				int MsFluxo = mboAcao.getInt("MSTBPEC_FLUXOID"); 					// Fluxo de Ação
-				int MsAcao = getMbo(row).getInt("MSACAO"); 							// Ação de Opções
-				String MsGrupo = mboGrupo.getString("MSGRUPO"); 					// Grupo de Grupos
-				
-				System.out.println("-------------------- PEC | PO/Fluxo/Ação/Grupo: ----> " + PoNum + " / " + MsFluxo + " / " +  MsAcao + " / " +  MsGrupo);
-				
-				// Setando Valores na PO
-				
-				MboRemote mboPO = app.getDataBean("MAINRECORD").getMbo();
-					
-				mboPO.setValue("MSPECACAO", MsAcao);
-				mboPO.setValue("MSPECFLUXO", MsFluxo);
-				mboPO.setValue("MSPECGRUPO", MsGrupo);
-				
-				System.out.println("-------------------- PEC | Valores Setados na PO: ----> " + PoNum + " / " + MsFluxo + " / " +  MsAcao + " / " +  MsGrupo);
-				
+
+				PoNum = app.getDataBean("MAINRECORD").getMbo().getInt("PONUM"); 
+				MsFluxo = mboAcao.getInt("MSTBPEC_FLUXOID");
+				MsAcao = getMbo(row).getInt("MSACAO"); 
+				MsGrupo = mboGrupo.getString("MSGRUPO"); 		
+	}
+
+	private void fecharERotear() throws RemoteException, MXException {
+
 		// Fechar Dialog ACTION
 
-		WebClientEvent closeEvt = new WebClientEvent("dialogok", this.app.getCurrentPageId(), null, this.clientSession);
+		WebClientEvent closeEvt = new WebClientEvent("dialogok",
+				this.app.getCurrentPageId(), null, this.clientSession);
 
 		WebClientRuntime.sendEvent(closeEvt);
 
 		// Auto Iniciar WORKFLOW
-		super.selectrecord();
 
 		DataBean ownerBean = this.app.getDataBean();
+		
 		if (ownerBean != null) {
 			MboRemote mbo = ownerBean.getMbo();
 
 			WebClientSession wcs = this.clientSession;
 			WorkflowDirector director = wcs.getWorkflowDirector();
 			director.allowAutoInit();
-			director.startInput(wcs.getCurrentAppId(), mbo, DirectorInput.workflow);
+			director.startInput(wcs.getCurrentAppId(), mbo,	DirectorInput.workflow);
+		}
+
+		save();
+
+	}
+	
+	private void validarCampos() throws MXException, RemoteException  {
+		System.out.println("applyCustomAction");
+
+		MboRemote aMbo;
+		MboRemote bMbo;
+		
+		MboSet mboCampos;
+		mboCampos = (MboSet) psdi.server.MXServer.getMXServer().getMboSet("MSTBPEC_CAMPOS", getMbo().getUserInfo());
+		mboCampos.setWhere("MSTBPEC_OPCOESID in (SELECT MSTBPEC_OPCOESID FROM MSTBPEC_OPCOES WHERE MSACAO = '"+MsAcao+"')");
+		mboCampos.reset();
+		
+		int retorno = 0;
+		
+		// Se Existir Campos Obrigatórios
+		if (mboCampos.count() > 0) {
+		
+			for (int i = 0; (aMbo = mboCampos.getMbo(i)) != null; i++) {
+				
+				System.out.println("Campo Obrigatório " + i + ":" + aMbo.getString("MSALNCAMPOS"));
+				
+				String Campo = aMbo.getString("MSALNCAMPOS");
+				String DescCampo = aMbo.getString("DESCRIPTION");
+				String Objeto = aMbo.getString("MSRELACIONAMENTO");
+				
+				
+				MboSet mboPO;
+				mboPO = (MboSet) app.getDataBean("MAINRECORD").getMbo().getMboSet(Objeto);
+				
+				int jTamanho = mboPO.count();
+				
+					for (int j = 0; j < jTamanho; j++) {
+						if (mboPO.getMbo(j).isNull(Campo)){
+							setResultadoCampoObrigatorio(DescCampo);
+							retorno++;
+						}
+					
+				}
+				
+				
+			}
 		}
 		
-		save();
+		if (app.getDataBean("MAINRECORD").getMbo().isNull("MSAREADAF")){
+			app.getDataBean("MAINRECORD").getMbo().setValue("MSFLAGMSG", "1", MboConstants.NOACCESSCHECK);
+    		save();
+    		throw new MXApplicationException("ms_clpo01", "informeAreaDaf");
+		}
 		
-		return 1;
+		if (retorno > 0){
+			
+	        if (!camposObrigatorios.equals("")) {
+	        		app.getDataBean("MAINRECORD").getMbo().setValue("MSFLAGMSG", "1", MboConstants.NOACCESSCHECK);
+	        		save();
+ 		            throw new MXApplicationException("ms_clpo01", "camposObrigatorios", new String[]{camposObrigatorios});
+	        }
+			
+		}
+		
+		app.getDataBean("MAINRECORD").getMbo().setValue("MSFLAGMSG", "0", MboConstants.NOACCESSCHECK);
+		save();
+	}
+
+	private void setarValores() throws RemoteException, MXException {
+
+		MboRemote mboPO = app.getDataBean("MAINRECORD").getMbo();
+
+		mboPO.setValue("MSPECACAO", MsAcao);
+		mboPO.setValue("MSPECFLUXO", MsFluxo);
+		mboPO.setValue("MSPECGRUPO", MsGrupo);
+
+	}
+
+	private void setResultadoCampoObrigatorio(String string) {
+		if (string.equals("")) {
+			camposObrigatorios = "";
+		} else {
+			if (camposObrigatorios.indexOf(string) == -1) {
+				camposObrigatorios += "\n" + string;
+			}
+		}
+	}
+
+	public String toWhereClause(Object arg0, MboSetRemote arg1)
+			throws MXException, RemoteException {
+		return "";
 	}
 
 }
