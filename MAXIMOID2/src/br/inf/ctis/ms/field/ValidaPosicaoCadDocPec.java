e8cf4158a7497b933f7ec2e3ab97e00ae4ace1c0
andre.engenheirodf@gmail.com <andre.engenheirodf@gmail.com@80753d7a-18a9-1890-8868-072d0b567191>
2014-10-06 19:16:48 +0000

    teste

---
+++ MAXIMOID2/src/br/inf/ctis/ms/field/ValidaPosicaoCadDocPec.java
@@ -0,0 +1,61 @@
+package br.inf.ctis.ms.field;
+
+import java.rmi.RemoteException;
+import java.util.ArrayList;
+import java.util.Calendar;
+
+import psdi.mbo.MAXTableDomain;
+import psdi.mbo.MboConstants;
+import psdi.mbo.MboRemote;
+import psdi.mbo.MboSet;
+import psdi.mbo.MboSetRemote;
+import psdi.mbo.MboValue;
+import psdi.server.MXServer;
+import psdi.util.MXApplicationException;
+import psdi.util.MXApplicationYesNoCancelException;
+import psdi.util.MXException;
+import br.inf.id2.common.util.Uteis;
+import br.inf.id2.common.util.Validar;
+
+public class ValidaPosicaoCadDocPec extends MAXTableDomain {
+
+	public ValidaPosicaoCadDocPec(MboValue mbv) {
+		super(mbv);
+		System.out.println(">>>>>>>>>>>>>>>>>>>> Dentro da Classe ValidaPosicaoCadDocPec versao 00 ");
+	}
+	
+	public void validate() throws MXException, RemoteException {
+		
+		if (getMboValue().isNull()) {
+            System.out.println(">>>>>>>>>>>>>>>>>>>> is null");
+            return;
+        }
+		
+		
+		System.out.println(">>>>>>>>>>>>>>>>>>>> Dentro do validate");
+
+		MboSet mbosetPosicao = (MboSet) getMboValue().getMbo().getMboSet("MSVWCLAUSULAPEC");
+        
+        System.out.println(">>>>>>>>>>>>>>>>>>>> Quantidade de registros na MSVWCLAUSULAPEC: "+mbosetPosicao.count());
+        
+        for (int i = 0; i < mbosetPosicao.count(); i++) {
+           
+                                          
+	            if (getMboValue().getString().equals(mbosetPosicao.getMbo(i).getString("MSPOSICAO"))) {
+	                
+	            	System.out.println(">>>>>>>>>>>>>>>>>>>> Dentro do IF para validar a existencia da Posicao");
+	            	                
+	                
+	                throw new MXApplicationException("genericaDoc", "PosicaoDocPecExiste");
+	            }
+	            else{
+	            	getMboValue().getMbo().setValue("MSPOSICAO", mbosetPosicao.getMbo(i).getString("MSPOSICAO"));
+	            	System.out.println(">>>>>>>>>>>>>>>>>>>> Dentro do else ja persistindo a Posicao");
+	            }
+        }
+        
+       
+
+	}
+
+ }
